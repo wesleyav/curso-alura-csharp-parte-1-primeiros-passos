a70536807ec5f403b2de529c279e1de903ccd3fd
Wesley <wesley.contato@hotmail.com>
2020-11-12 05:36:30 -0300

    Caracteres e textos

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e textos");
+
+            // character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2020;
+            string cursosProgramacao = 
+@" 
+- .NET 
+- JAVA   
+- JavaScript";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
